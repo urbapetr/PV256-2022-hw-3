efc6a559ffc39602cf06d8750802321a6df7353e
Petr Urbánek <514122@mail.muni.cz>
2022-03-10 15:41:25 +0100

    homework finished

--- app/src/main/kotlin/cz/muni/fi/pv256/hw3/MainActivity.kt
+++ app/src/main/kotlin/cz/muni/fi/pv256/hw3/MainActivity.kt
@@ -1,9 +1,9 @@
 package cz.muni.fi.pv256.hw3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
