5092b0478335b1b60525961e49463a38359f90b9
Petr Urbánek <514122@mail.muni.cz>
2022-03-10 14:49:49 +0100

    homework finished

--- app/src/main/kotlin/cz/muni/fi/pv256/hw3/MainActivity.kt
+++ app/src/main/kotlin/cz/muni/fi/pv256/hw3/MainActivity.kt
@@ -1,16 +1,47 @@
 package cz.muni.fi.pv256.hw3
 
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.properties.Delegates
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private var count by Delegates.notNull<Int>()
+    private val COUNT_KEY = "COUNT_KEY"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // TODO initialise TextView and Buttons as local values
+        // TODO 2. initialise TextView and Buttons as local values
+        val decrement = findViewById<Button>(R.id.decrement)
+        val increment = findViewById<Button>(R.id.increment)
+        val textView = findViewById<TextView>(R.id.counter)
+
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt(COUNT_KEY)
+        }
+        else{
+            count = 0
+        }
+        textView.text = count.toString()
+
+        // TODO 3. Implement counter using TextView and the field `count`
+        decrement.setOnClickListener {
+            count--
+            textView.text = count.toString()
+        }
+        increment.setOnClickListener {
+            count++
+            textView.text = count.toString()
+        }
+    }
+
+    // TODO 4. Make sure the counter survives orientation changes (its value won't reset or change on screen rotation)
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(COUNT_KEY, count)
     }
 }
